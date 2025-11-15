d0c49b2fd20c920bd76a400741022a17dd72beb7
Romain Harlaut <romain.harlaut.etu@univ-lille.fr>
2025-11-15 22:06:27 +0100

    gitignore + module pour clean le css

---
+++ purgecss.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  content: ['./*.html'], // tous tes fichiers HTML
+  css: ['./css/*.css'],  // ton fichier CSS
+  output: './css/clean/', // dossier où générer le CSS nettoyé
+};
