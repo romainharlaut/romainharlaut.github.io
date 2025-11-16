92a3434b0ef0347cf7f0e745ac96ec4fa4545d9e
Romain Harlaut <romain.harlaut.etu@univ-lille.fr>
2025-11-16 14:34:52 +0100

    css purge

--- purgecss.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  content: ['./*.html'], // tous tes fichiers HTML
-  css: ['./css/*.css'],  // ton fichier CSS
-  output: './css/clean/', // dossier où générer le CSS nettoyé
-};
