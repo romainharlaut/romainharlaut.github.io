fe3ef97653366979836161c634536518378db4f0
Romain Harlaut <romain.harlaut.etu@univ-lille.fr>
2025-11-22 21:16:04 +0100

    fix pour pas affiche informations du formulairedans url

--- js/contact.js
+++ js/contact.js
@@ -13,22 +13,17 @@ document.addEventListener("DOMContentLoaded", function () {
     return;
   }
 
-
   form.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    form.addEventListener("submit", function (event) {
-      event.preventDefault();
-
-      emailjs.sendForm("service_2cyfkjs","template_i5r735q",form)
-        .then(() => {
-          alert("✅ Message envoyé avec succès !");
-          form.reset();
-        })
-        .catch(error => {
-          console.error("❌ Erreur :", error);
-          alert("Erreur : " + JSON.stringify(error));
-        });
-    });
+    emailjs.sendForm("service_2cyfkjs", "template_i5r735q", form)
+      .then(() => {
+        alert("✅ Message envoyé avec succès !");
+        form.reset();
+      })
+      .catch(error => {
+        console.error("❌ Erreur :", error);
+        alert("Erreur : " + JSON.stringify(error));
+      });
   });
 });
