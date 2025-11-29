f5f27c104dae4922bdeb68e82b8655dc6e81f5d3
Romain Harlaut <romain.harlaut.etu@univ-lille.fr>
2025-11-29 13:52:09 +0100

    fix cookie

--- js/cookies.js
+++ js/cookies.js
@@ -1,17 +1,19 @@
+window.onload = function() {
+    tarteaucitron.init({
+        "privacyUrl": "",
+        "hashtag": "#tarteaucitron",
+        "cookieName": "tarteaucitron",
+        "orientation": "bottom",
+        "showAlertSmall": true,
+        "cookieslist": true,
+        "adblocker": false,
+        "AcceptAllCta": true,
+        "highPrivacy": true,
+        "handleBrowserDNTRequest": false,
+        "googleConsentMode": true
+    });
 
-tarteaucitron.init({
-    "hashtag": "#tarteaucitron",
-    "cookieName": "tarteaucitron",
-    "orientation": "bottom",
-    "showAlertSmall": true,
-    "cookieslist": true,
-    "adblocker": false,
-    "AcceptAllCta": true,
-    "highPrivacy": true,
-    "handleBrowserDNTRequest": false,
-    "googleConsentMode": true
-});
-
-tarteaucitron.user.gtagUa = 'G-4WC1W6F4MT';
-tarteaucitron.user.gtagMore = function () {};
-(tarteaucitron.job = tarteaucitron.job || []).push('gtag');
+    tarteaucitron.user.gtagUa = 'G-4WC1W6F4MT';
+    tarteaucitron.user.gtagMore = function () {};
+    (tarteaucitron.job = tarteaucitron.job || []).push('gtag');
+};
