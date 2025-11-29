d9eb882766543ee2aca927d351ede354d7d7f64b
Romain Harlaut <romain.harlaut.etu@univ-lille.fr>
2025-11-29 13:19:53 +0100

    ajout cookie pour analyse traffic

---
+++ js/cookies.js
@@ -0,0 +1,17 @@
+
+tarteaucitron.init({
+    "hashtag": "#tarteaucitron",
+    "cookieName": "tarteaucitron",
+    "orientation": "bottom",
+    "showAlertSmall": true,
+    "cookieslist": true,
+    "adblocker": false,
+    "AcceptAllCta": true,
+    "highPrivacy": true,
+    "handleBrowserDNTRequest": false,
+    "googleConsentMode": true
+});
+
+tarteaucitron.user.gtagUa = 'G-4WC1W6F4MT';
+tarteaucitron.user.gtagMore = function () {};
+(tarteaucitron.job = tarteaucitron.job || []).push('gtag');
